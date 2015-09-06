2b23546dbd646027c46bc06e9bc002d1dd9d2f38
Milton Becker Junior <miltonbecker@gmail.com>
2015-09-06 19:30:12 -0300

    playing

--- hello-world.rb
+++ hello-world.rb
@@ -1,1 +1,17 @@
-puts 'cbb'
+class Plane
+
+  attr_accessor :something
+
+  def initialize(speed = 100)
+    @speed = speed
+  end
+
+  def fly
+    puts "Flying at #{@speed} km/h and #{@something} and " + :bla.to_s
+  end
+
+end
+
+obj = Plane.new
+obj.something = "yeah"
+obj.fly
